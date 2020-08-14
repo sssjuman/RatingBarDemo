dff7ad5cba612dd3d39d01c0fec203e0e1015c6d
sssjuman <samshen030024@gmail.com>
2020-08-14 10:07:30 +0800

    Initialization

---
+++ app/src/main/java/com/example/ratingbardemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ratingbardemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                String stars = v + "star(s)";
+                Toast.makeText(MainActivity.this, stars, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
